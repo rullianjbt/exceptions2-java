286c6aafd533ff388ea2a5c63c547acd4328b5ae
Rullian Siqueira da Mata <rullianjbt@gmail.com>
2021-02-22 09:59:28 -0300

    Exercise using custom exceptions to solve problems in Java

---
+++ src/application/Program.java
@@ -0,0 +1,43 @@
+package application;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+import model.entities.Account;
+import model.exceptions.DomainException;
+
+public class Program {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		Locale.setDefault(Locale.US);
+
+		System.out.println("Enter account data");
+		System.out.println("Number:");
+		int number = scan.nextInt();
+		System.out.println("Holder:");
+		String holder = scan.nextLine();
+		System.out.println("Initial balance:");
+		Double initialBalance = scan.nextDouble();
+		System.out.println("Withdraw limit:");
+		Double withdrawLimit = scan.nextDouble();
+		Account acc = new Account(number, holder, initialBalance, withdrawLimit);
+		
+		System.out.println("Enter amount for withdraw:");
+		double ammount = scan.nextDouble();
+		
+		try {
+			acc.withdraw(ammount);
+			System.out.println("New balance: "+String.format("%.2f", acc.getBalance()));
+		}
+		
+		catch (DomainException e){
+			System.out.println("Withdraw error: "+e.getMessage());
+		}
+		
+		scan.close();
+		
+
+	}
+
+}
